dc19896802d9e8ca9b5430d0043a0938ae1da68d
nagachen <birdann99@gmail.com>
2023-06-26 20:27:15 +0800

    完成class 中的save()及del()

--- base.php
+++ base.php
@@ -46,16 +46,42 @@ class DB
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
     function save($arg){
+        if(isset($arg['id'])){
+            //update
+            foreach ($arg as $key => $value) {
+                $tmp[] = "`$key` = '$value'";
+            }
+            $sql ="update $this->table set" .join(',',$tmp) ."where `id` = '{$arg['id']}'";
+        }else{
+            //insert
+            $cols=array_keys($arg);
+            $sql="insert into $this->table(`".join("`,`",$cols)."`)
+                    values('".join("','",$arg)."')";
+        }
+        return $this->pdo->exec($sql);
+        }
+    
+    function del($arg){
+        $sql="delete from $this->table";
+        if (is_array($arg)) {
+            foreach ($arg as $key => $value) {
+                $tmp[] = "`$key` = '$value'";
+            }
+            $sql = $sql . "where" . join("&&", $tmp);
+        } else {
+            $sql = $sql . $arg;
+        }
 
     }
-    function count(...$arg){
 
+    function count(...$arg){
+        
     }
     function math($math,$cols,...$arg){
 
     }
-}
 
+}
 function dd($array){
     echo "<pre>";
     print_r($array);
