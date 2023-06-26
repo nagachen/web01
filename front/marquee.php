152a3e31f56048526e111dac3ed42a65fc823195
nagachen <birdann99@gmail.com>
2023-06-26 18:49:26 +0800

    建立前台三個頁面的切版,使用GET內容來載入不同功能的頁面

---
+++ front/marquee.php
@@ -0,0 +1,4 @@
+<marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
+	HIIIIIIIIIIIII
+
+</marquee>
--- index.php
+++ index.php
@@ -36,7 +36,17 @@
 						1 </span>
 				</div>
 			</div>
-			<?php include "./front/main.php" ?>
+			<?php 
+			$do=$_GET['do']??'main';//使用三元運算式來取得網址的GET參數
+			$file="./front/".$do.".php"; //建立檔案路徑及檔名
+			//判斷檔案是否存在,存在則載入,不存在則載入main.php
+			if(file_exists($file)){
+				include $file;
+			}else{
+				include "./front/main.php";
+			}
+
+			?>
 			<div id="alt"
 				style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
 			</div>
