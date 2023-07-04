5e02aa0f0e4ed532b4831f0b5f86926689b58249
nagachen <birdann99@gmail.com>
2023-07-04 21:51:06 +0800

    完成編輯與刪除次選單功能

--- api/submenu.php
+++ api/submenu.php
@@ -2,6 +2,20 @@
 include_once "../base.php";
 $main_id=$_POST['main_id'];
 dd($_POST);
+//編輯/刪除次選單
+if(isset($_POST['text'])){
+    foreach($_POST['text'] as $id=>$text){
+        if(isset($_POST['del'])&& in_array($id,$_POST['del'])){
+            $Menu->del($id);
+        }else{
+            $row=$Menu->find($id);
+            $row['text']=$text;
+            $row['href']=$P_OST['href'][$id];
+            $Menu->save($row);
+        }
+    }
+}
+//新增次選單
 if(isset($_POST['text2'])){
     foreach($_POST['text2'] as $idx => $text){
         if($text!=""){
